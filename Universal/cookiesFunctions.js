959e442e9c1ae1427df4e1cdb3101bb7d51cf058
Trevor Spear <TrevorSpear@Rebeccas-MacBook-Pro.local>
2017-05-26 17:48:00 -0700

    Basic layout of the files

---
+++ Universal/cookiesFunctions.js
@@ -0,0 +1,13 @@
+/**
+ * Created by TrevorSpear on 5/26/17.
+ */
+
+//Get the cookie and greet user by it
+
+
+//Get score
+
+
+//Anything else
+
+
---
+++ Universal/websiteBars.js
@@ -0,0 +1,24 @@
+/**
+ * Created by TrevorSpear on 5/26/17.
+ */
+
+//TopBar - NavBar in every page
+function topBar() {
+    //Div
+        //Home
+        //Games - Drop Down
+        //Music
+        //Sign Up
+        //Sign In / Login - These can be on a drop down ^
+        //About - Drop down
+
+}
+
+//BottomBar - Bottom info bar that tells who made the site
+function bottomBar() {
+    //Div
+        //Any of the following but just one
+        //By: _________ and ___________
+        //Group: ______________
+        //Something else
+}
